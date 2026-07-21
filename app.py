aada8549cc2afce84ece698adcfc5c5dcbd5c991
Anderson K <andersonkho27@gmail.com>
2026-07-21 00:35:19 +0000

    Added hobbies

--- app.py
+++ app.py
@@ -18,5 +18,25 @@ def aboutme():
     return render_template("aboutme.html")
 
 
+@app.route("/myprojects")
+def myprojects():
+    return render_template("myprojects.html")
+
+
+@app.route("/rpmcounter")
+def rpmcounter():
+    return render_template("rpmcounter.html")
+
+
+@app.route("/spinningtop")
+def spinningtop():
+    return render_template("spinningtop.html")
+
+
+@app.route("/hobbies")
+def hobbies():
+    return render_template("hobbies.html")
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
